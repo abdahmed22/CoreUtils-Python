66409c9d4c455a990dda26d7c51f2406417769ce
abdahmed22 <abd.ahmed03@eng-st.cu.edu.eg>
2024-07-01 16:30:12 +0300

    Feature: yes program

--- cmd/yes/yes.py
+++ cmd/yes/yes.py
@@ -1,24 +1,13 @@
-package main
+import sys
 
-import (
-	"fmt"
-	"os"
-)
+temp = 10
+args = sys.argv[1:]
 
-func main() {
-
-	args := os.Args[1:]
-
-	if len(args) != 0 {
-		for {
-			for i := range len(args) {
-				fmt.Printf("%s ", args[i])
-			}
-			fmt.Printf("\n")
-		}
-	} else {
-		for {
-			fmt.Printf("y\n")
-		}
-	}
-}
+if len(args) == 0:
+	while temp > 0 :
+		print("y")
+else:
+	while temp > 0 :
+		for word in args:
+			print(word, end =" ")
+		print()
